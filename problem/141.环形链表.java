a5a08862daf49a97ff551b890adfde5a6ea3f109
weijiew <836678589@qq.com>
2020-12-04 00:30:04 +0800

    add

---
+++ problem/141.环形链表.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode a = head , b = head.next;
+        while (b != null && b.next != null) {
+            if (a == b) return true;
+            a = a.next;
+            b = b.next.next;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
--- problem/160.相交链表.java
+++ problem/160.相交链表.java
@@ -18,7 +18,12 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-                   
+        ListNode a = headA , b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }        
+        return a;
     }
 }
 // @lc code=end
